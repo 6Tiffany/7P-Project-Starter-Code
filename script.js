5b1a726901a3674098259526c75e1ae9c85dff14
6Tiffany <104591521+6Tiffany@users.noreply.github.com>
2022-04-28 11:39:08 -0700

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,15 @@
+$("button").click(function() {
+let variable1 = $(".A").val();
+let variable2 = $(".B").val();
+let variable3 = $(".C").val();
+console.log(variable1);
+console.log(variable2);
+console.log(variable3);
+    
+$(".fortune-display").append(variable1 + " Tiffany" + variable2 + " you will use glass cup drinking water" + variable3 + " you will  drinking water 11648000 ml water.");
+
+});
+
+$(".Y").click(function() {
+    $(".CC").show();
+});
